aed8f2b8f484df9eafb2eaf421f8fa5a83a4c6fb
rocharicardo9 <ricardorocha9@gmail.com>
2024-03-29 22:43:07 -0300

    Inclui no projeto uma Imagem

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-$(document).ready(function() {
-    $('#telefone').mask('(00) 00000-0000');
-    $('#cpf').mask('000.000.000-00', {reverse: true});
-    $('#cep').mask('00000-000');
-  
-    $('#cadastroForm').submit(function(event) {
-      event.preventDefault();
-      
-      var formData = $(this).serialize();
-      console.log(formData);
-      $.post('processar_formulario.php', formData, function(response) {
-      console.log(response);
-      });
-    });
-  });
-  
+$(document).ready(function(){
+  $('#telefone').mask('(00) 00000-0000');
+  $('#cpf').mask('000.000.000-00');
+  $('#cep').mask('00000-000');
+});
